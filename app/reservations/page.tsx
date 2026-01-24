c640b7f1cdb8db2423a586e821e808641192f94a
Deybi Jimenez <deybijimenez94@gmail.com>
2026-01-23 19:41:32 -0700

    Update site

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: '/images/Interior/Inside1.jpg',
+        url: 'https://bearstreettavern.ca/images/Interior/Inside1.jpg',
         width: 1200,
         height: 630,
         alt: 'Interior Dining - Reservations',
